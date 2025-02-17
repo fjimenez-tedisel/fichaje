5f97ba005994837b42856d920235dbf950a7b24e
fjimenez-tedisel <fjimenez@tedisel.com>
2025-02-17 22:26:30 +0100

    Creacion calendario

--- fichaje-front/src/app/pages/vacaciones/vacaciones.component.ts
+++ fichaje-front/src/app/pages/vacaciones/vacaciones.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-vacaciones',
   standalone: true,
-  imports: [CalendarModule, FormsModule],
+  imports: [CalendarModule, FormsModule,   MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule],
   templateUrl: './vacaciones.component.html',
   styleUrl: './vacaciones.component.css'
 })
 export class VacacionesComponent {
-  date!: Date; // Se almacenará la fecha seleccionada
+  selectedDates: Date[] = [];
+
 }
