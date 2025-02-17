8ab43fd40ee3baea83649d98d5229d31c768e21d
fjimenez-tedisel <fjimenez@tedisel.com>
2025-02-17 17:12:29 +0100

    Modificación del interfaz de fichajes

--- fichaje-front/src/app/pages/fichaje/fichaje.component.ts
+++ fichaje-front/src/app/pages/fichaje/fichaje.component.ts
@@ -1,18 +1,33 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-fichaje',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './fichaje.component.html',
   styleUrl: './fichaje.component.css'
 })
 export class FichajeComponent {
   ultimaFichada: string = '';
+  fechaActual!: Date;
+  entryHour: string = "";
+  finishHour: string = "";
+
+  constructor() {
+    setInterval(() => {
+      this.fechaActual = new Date();
+    }, 1000);
+  }
   
-  fichar() {
-    const ahora = new Date();
-    this.ultimaFichada = ahora.toLocaleString(); // Guarda la fecha y hora actual
+  setEntry() {
+    const now = new Date();
+    this.entryHour = now.toLocaleString(); // Guarda la fecha y hora actual
+  }
+  setFinish() {
+    const now = new Date();
+    this.finishHour= now.toLocaleString(); // Guarda la fecha y hora actual
   }
 }
 
